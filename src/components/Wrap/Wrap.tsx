a6c3075cba9002d40048050bf76628d9f5542a8e
Jamiu Ibikunle <ibidotun@gmail.com>
2023-05-06 23:34:40 +0100

    handled mobile for wrap

--- src/components/Wrap/Wrap.tsx
+++ src/components/Wrap/Wrap.tsx
@@ -4,13 +4,11 @@ import useMobile from "../../utils/useMobile";
 const Wrap = ({ children }) => {
   const [isMobile] = useMobile(768);
 
-  console.log(isMobile);
-
   const main = {
     width: "100%",
-    height: "100vh",
+    height: isMobile ? "100%" : "100vh",
     display: "flex",
-    flexDirection: "row",
+    flexDirection: isMobile ? "column" : "row",
   };
 
   return <div style={{ ...main } as React.CSSProperties}>{children}</div>;
