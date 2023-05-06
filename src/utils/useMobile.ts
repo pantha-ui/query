b5fcd2404386941dcbe940c8ee057f96d6e29efc
Jamiu Ibikunle <ibidotun@gmail.com>
2023-05-06 23:29:53 +0100

    wrote usemobile custom hook

---
+++ src/utils/useMobile.ts
@@ -0,0 +1,13 @@
+import React from "react";
+
+const useMobile = (num: number) => {
+  const [currentWidth, setCurrentWidth] = React.useState<number>(window.innerWidth);
+
+  window.addEventListener("resize", () => {
+    setCurrentWidth(window.innerWidth);
+  });
+
+  return currentWidth >= num ? [true] : [false];
+};
+
+export default useMobile;
